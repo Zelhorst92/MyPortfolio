b691a3b5195b6f9fed788cc24f510146c3bd0426
Zelhorst92 <r.zelhorst@gmail.com>
2022-02-27 13:16:43 +0000

    Customize admin Services

--- services/admin.py
+++ services/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Service, Category
 
 # Register your models here.
-admin.site.register(Service)
-admin.site.register(Category)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Service, ServiceAdmin)
