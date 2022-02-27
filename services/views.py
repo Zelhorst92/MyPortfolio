c3f47198edfff3e4429bdb5fcee567d698239656
Zelhorst92 <r.zelhorst@gmail.com>
2022-02-27 15:46:50 +0000

    Add initial services template plus links

--- services/views.py
+++ services/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
+
+
+def all_services(request):
+    """View to show all services for sale"""
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
