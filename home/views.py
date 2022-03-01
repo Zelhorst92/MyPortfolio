5213e12a79dc9fad2de799a7caaead7dc09d5e66
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-01 18:58:26 +0000

    Show services on homepage

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Skill, PreviousProject
+from services.models import Service
 
 # Create your views here.
 
@@ -8,10 +9,12 @@ def index(request):
     """View to return index page"""
     skills = Skill.objects.all()
     PreviousProjects = PreviousProject.objects.all()
+    services = Service.objects.all()
 
     context = {
         'skills': skills,
         'PreviousProjects': PreviousProjects,
+        'services': services,
     }
 
     return render(request, 'home/index.html', context)
