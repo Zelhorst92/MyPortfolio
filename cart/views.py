59d650b5eaa7b09d0006c070e06a3c15d48ef35c
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-05 17:26:29 +0000

    Add messages in cart view + minor styling toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from services.models import Service
@@ -14,7 +14,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """Add a quantity of a service to the shopping cart"""
 
-    service = Service.objects.get(pk=item_id)
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -33,13 +33,16 @@ def add_to_cart(request, item_id):
 def update_cart(request, item_id):
     """change the quantity of a service in the shopping cart"""
 
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Quantity of {service.name} updated to {quantity}!')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {service.name} from the cart!')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -49,11 +52,15 @@ def remove_from_cart(request, item_id):
     """Remove a service from the shopping cart"""
 
     try:
+        service = get_object_or_404(Service, pk=item_id)
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(request, f'Removed all {service.name} from the cart!')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
+
     except Exeption as e:
+        messages.error(request, f'Error removing the item: {e}')
         return HttpResponse(status=500)
