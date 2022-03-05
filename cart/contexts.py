1236d70ebf7051c9e5867afd3c0957af6961c93f
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-05 11:12:45 +0000

    Expand cart functionality + styling

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,25 +1,39 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service
 
 
 def cart_contents(request):
+    """Context processor for shopping cart"""
 
     cart_items = []
     total = 0
     cart_item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        total += quantity * service.price
+        cart_item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
 
     if cart_item_count > 1:
-        net_total = total * Decimal(
-            settings.COMBINATION_DISCOUNT_PERCENTAGE / 100)
+        discount = total * Decimal(settings.COMBINATION_DISCOUNT_PERCENTAGE / 100)
     else:
-        net_total = total
+        discount = 0
 
     context = {
         'cart_items': cart_items,
         'total': total,
         'cart_item_count': cart_item_count,
         'combination_discount_percentage': settings.COMBINATION_DISCOUNT_PERCENTAGE,
-        'net_total': net_total,
+        'discount': discount,
+        'net_total': (total - discount),
     }
 
     return context
