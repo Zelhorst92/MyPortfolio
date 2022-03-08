00ce5f81586e2ac41f92df43763f4b512d10245f
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-08 14:26:44 +0000

    Add delete services functionality + securing appropriate views

--- services/views.py
+++ services/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Service
 from .forms import ServiceForm
 
 # Create your views here.
-
-
-def all_services(request):
-    """View to show all services for sale"""
-
-    services = Service.objects.all()
-
-    context = {
-        'services': services,
-    }
-
-    return render(request, 'services/services.html', context)
 
 
 def service_details(request, service_id):
@@ -29,8 +18,30 @@ def service_details(request, service_id):
 
     return render(request, 'services/service_details.html', context)
 
+
+@login_required
+def all_services(request):
+    """View to show all services for sale"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to view this.')
+        return redirect(reverse('home'))
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
+
+
+@login_required
 def add_service(request):
     """Add a service"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ServiceForm(request.POST, request.FILES)
         if form.is_valid():
@@ -51,8 +62,13 @@ def add_service(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_service(request, service_id):
     """Edit a service"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to do that.')
+        return redirect(reverse('home'))
+
     service = get_object_or_404(Service, pk=service_id)
     if request.method == 'POST':
         form = ServiceForm(request.POST, request.FILES, instance=service)
@@ -74,3 +90,15 @@ def edit_service(request, service_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_service(request, service_id):
+    """Delete a service"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to do that.')
+        return redirect(reverse('home'))
+
+    service = get_object_or_404(Service, pk=service_id)
+    service.delete()
+    messages.success(request, 'Service successfully deleted!')
