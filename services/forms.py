3c013cd67c1756b674363f41f87bd92b9f6295a1
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-08 15:20:38 +0000

    Update styling to services pages

--- services/forms.py
+++ services/forms.py
@@ -15,4 +15,4 @@ class ServiceForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'rounded-0'
+            field.widget.attrs['class'] = 'form-input rounded-0'
