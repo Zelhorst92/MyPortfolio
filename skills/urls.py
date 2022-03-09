b4d070dbf5ecc5a8eb1fd7cb713dc352e14621b3
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-09 20:43:33 +0000

    Add overview, add and edit skill functionality

--- skills/urls.py
+++ skills/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.skills, name='skills'),
+    path('', views.all_skills, name='skills'),
+    path('add/', views.add_skill, name='add_skill'),
+    path('edit/<int:skill_id>', views.edit_skill, name='edit_skill'),
 ]
--- skills/views.py
+++ skills/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Skill
+from .forms import SkillForm
 
 # Create your views here.
 
 
-def skills(request):
+@login_required
+def all_skills(request):
+    """View to show all the skills in the database"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to view this.')
+        return redirect(reverse('home'))
+
     skills = Skill.objects.all()
 
     context = {
@@ -11,3 +21,59 @@ def skills(request):
     }
 
     return (render(request, 'skills/skills.html', context))
+
+@login_required
+def add_skill(request):
+    """Add a skill"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = SkillForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Skill succesfully added!')
+            return redirect(reverse('skills'))
+        else:
+            messages.error(request, 'Failed to add skill. \
+                Are all fields filled in correctly?')
+    else:
+        form = SkillForm()
+
+    template = 'skills/add_skill.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_skill(request, skill_id):
+    """Edit a skill"""
+    if not request.user.is_superuser:
+        messages.error(request, 'You have no permission to do that.')
+        return redirect(reverse('home'))
+
+    skill = get_object_or_404(Skill, pk=skill_id)
+    if request.method == 'POST':
+        form = SkillForm(request.POST, request.FILES, instance=skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Skill successfully edited!')
+            return redirect(reverse('skills'))
+        else:
+            messages.error(request, 'Failed to edit skill. \
+                Are all fields filled in correctly?')
+    else:
+        form = SkillForm(instance=skill)
+        messages.info(request, f'You are editing skill {skill.name}')
+
+    template = 'skills/edit_skill.html'
+    context = {
+        'form': form,
+        'skill': skill,
+    }
+
+    return render(request, template, context)
