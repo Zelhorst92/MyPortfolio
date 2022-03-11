5049439cb3edd509c26d215fd0e625f96664ed3c
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-11 14:25:28 +0000

    Add cache control

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -185,6 +185,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GTM',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # AWS Bucket configs
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.environ("AWS_S3_REGION_NAME")
