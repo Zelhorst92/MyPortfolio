8b74e57b0441eff06a4e67b63432147a704c891c
Zelhorst92 <r.zelhorst@gmail.com>
2022-03-12 15:18:02 +0000

    Add if statement db access in debug

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -127,17 +127,18 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('HEROKU_POSTGRES_DATABASE_URL'))
-}
+if 'DEBUG' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get('HEROKU_POSTGRES_DATABASE_URL'))
+    }
 
 
 # Password validation
